a3609af6c25927c5ecbb6f876fe38279cbf4db9d
HarpersGeekly <HarperRyanC@gmail.com>
2017-02-07 12:02:46 -0600

    Added post controller

--- src/main/java/com/example/controllers/PostsController.java
+++ src/main/java/com/example/controllers/PostsController.java
@@ -1,6 +1,4 @@
 package com.example.controllers;
-
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,26 +11,26 @@ public class PostsController {
 
     @GetMapping("/posts")
     @ResponseBody
-    public String postsIndexPage() {
+    public String viewAllPosts() {
         return "<h1>Posts index page</h1>";
     }
 
     @GetMapping("/posts/{id}")
     @ResponseBody
-    public String viewPost() {
-        return "<h1>view an individual post</h1>";
+    public String viewSinglePost(@PathVariable long id) {
+        return "<h1>view an individual post with ID: " + id + ".</h1>";
     }
 
+    // think of the next two as Step 1, and then Step 2 respectively...a doGet, doPost
     @GetMapping("/posts/create")
     @ResponseBody
-    public String viewForm() {
+    public String viewCreatePostForm() {
         return "<h1>view the form for creating a post</h1>";
     }
 
     @PostMapping("/posts/create")
     @ResponseBody
-    public String createPost() {
+    public String createNewPost() {
         return "<h1>create a new post</h1>";
     }
-
 }
